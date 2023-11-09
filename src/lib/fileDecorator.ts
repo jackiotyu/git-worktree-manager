bbfa09cc9fdf8e686dab96ead57726c39a3a83f2
jackhuang <2504448153@qq.com>
2023-11-09 10:49:38 +0800

    docs: WorkTreeDecorator
    Signed-off-by: jackhuang <2504448153@qq.com>

--- src/lib/fileDecorator.ts
+++ src/lib/fileDecorator.ts
@@ -1,32 +1,35 @@
 import * as vscode from 'vscode';
 import { WORK_TREE_SCHEME } from '@/constants';
 
+/**
+ * Fork from https://github.com/gitkraken/vscode-gitlens/blob/main/src/views/viewDecorationProvider.ts#L149
+ */
 export class WorkTreeDecorator implements vscode.FileDecorationProvider {
     provideFileDecoration(uri: vscode.Uri, token: vscode.CancellationToken) {
-        if(uri.scheme !== WORK_TREE_SCHEME) return undefined;
-		const [, , status] = uri.path.split('/');
+        if (uri.scheme !== WORK_TREE_SCHEME) return undefined;
+        const [, , status] = uri.path.split('/');
 
-		switch (status) {
-			case 'ahead':
-				return {
-					badge: '▲',
-					color: new vscode.ThemeColor('charts.green'),
-					tooltip: 'Ahead',
-				};
-			case 'behind':
-				return {
-					badge: '▼',
-					color: new vscode.ThemeColor('charts.orange'),
-					tooltip: 'Behind',
-				};
-			case 'diverged':
-				return {
-					badge: '▼▲',
-					color: new vscode.ThemeColor('charts.yellow'),
-					tooltip: 'Diverged',
-				};
-			default:
-				return undefined;
-		}
+        switch (status) {
+            case 'ahead':
+                return {
+                    badge: '▲',
+                    color: new vscode.ThemeColor('charts.green'),
+                    tooltip: 'Ahead',
+                };
+            case 'behind':
+                return {
+                    badge: '▼',
+                    color: new vscode.ThemeColor('charts.orange'),
+                    tooltip: 'Behind',
+                };
+            case 'diverged':
+                return {
+                    badge: '▼▲',
+                    color: new vscode.ThemeColor('charts.yellow'),
+                    tooltip: 'Diverged',
+                };
+            default:
+                return undefined;
+        }
     }
 }
