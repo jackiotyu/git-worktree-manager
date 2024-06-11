ad60e2c1fe6a046e18553d30dadedef02710ae3c
jackiotyu <2504448153@qq.com>
2024-06-11 22:04:54 +0800

    perf: 优化添加多个git文件夹的流程
    Signed-off-by: jackiotyu <2504448153@qq.com>

--- src/core/command/addGitFolderCmd.ts
+++ src/core/command/addGitFolderCmd.ts
@@ -1,13 +1,14 @@
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 import { Alert } from '@/core/ui/message';
-import folderRoot from "@/core/folderRoot";
-import path from "path";
+import folderRoot from '@/core/folderRoot';
+import path from 'path';
 import fs from 'fs/promises';
 import { toSimplePath } from '@/core/util/folder';
 import { getMainFolder } from '@/core/git/getMainFolder';
 import { getFolderConfig, updateFolderConfig } from '@/core/util/state';
 import { worktreeEventRegister } from '@/core/event/git';
 import { addToGitFolder } from '@/core/command/addToGitFolder';
+import { pickMultiFolder } from '@/core/ui/pickGitFolder';
 
 const addMultiGitFolder = async () => {
     let uriList = await vscode.window.showOpenDialog({
@@ -24,32 +25,34 @@ const addMultiGitFolder = async () => {
         .readdir(folderPath, { encoding: 'utf-8' })
         .then((files) => files.map((fileName) => path.join(folderPath, fileName)))
         .catch(() => []);
-    if(!files.length) return;
+    if (!files.length) return;
     const folders = await Promise.all(
         files.map(async (filePath) => {
             try {
-                return toSimplePath((await getMainFolder(filePath)));
+                return toSimplePath(await getMainFolder(filePath));
             } catch {
                 return null;
             }
         }),
     );
     const existFolders = getFolderConfig();
-    const distinctFolders = [...new Set(folders)];
+    const distinctFolders = [...new Set(folders.filter(i => i))];
+    if (!distinctFolders.length) {
+        Alert.showErrorMessage(vscode.l10n.t('There are no folders to add'));
+        return;
+    }
     const existFoldersMap = new Map(existFolders.map((i) => [toSimplePath(i.path), true]));
     const gitFolders = distinctFolders.filter((i) => i && !existFoldersMap.has(i)) as string[];
-    if (!gitFolders.length) return;
-    const options: vscode.QuickPickItem[] = gitFolders.map((folderPath) => ({ label: folderPath }));
-    const selected = await vscode.window.showQuickPick(options, {
-        canPickMany: true,
-        title: vscode.l10n.t('Select folder(s)'),
-    });
-    if (!selected) return;
-    const selectGitFolders = selected.map((item) => item.label);
+    if (!gitFolders.length) {
+        Alert.showErrorMessage(vscode.l10n.t('All folders have been added, there are no more folders to add'));
+        return;
+    }
+    const selectGitFolders = await pickMultiFolder(gitFolders);
+    if (!selectGitFolders || !selectGitFolders.length) return;
     const newFolders = getFolderConfig();
-    newFolders.push(...selectGitFolders.map((i) => ({ name: i, path: i })));
+    newFolders.push(...selectGitFolders);
     await updateFolderConfig(newFolders);
-    selectGitFolders.forEach((folderPath) => worktreeEventRegister.add(vscode.Uri.file(folderPath)));
+    selectGitFolders.forEach((item) => worktreeEventRegister.add(vscode.Uri.file(item.path)));
     Alert.showInformationMessage(vscode.l10n.t('Saved successfully'));
 };
 
@@ -72,13 +75,13 @@ export const addGitFolderCmd = async () => {
     const singleLabel = vscode.l10n.t('Single repository');
     let options: vscode.QuickPickItem[] = [
         { label: multiLabel, iconPath: new vscode.ThemeIcon('checklist') },
-        { label: singleLabel, iconPath: new vscode.ThemeIcon('repo') }
+        { label: singleLabel, iconPath: new vscode.ThemeIcon('repo') },
     ];
     let selected = await vscode.window.showQuickPick(options, {
         canPickMany: false,
         title: vscode.l10n.t('Add git repository'),
     });
-    if(!selected) return;
-    if(selected.label === multiLabel) addMultiGitFolder();
+    if (!selected) return;
+    if (selected.label === multiLabel) addMultiGitFolder();
     else addSingleGitFolder();
 };
--- src/core/ui/pickGitFolder.ts
+++ src/core/ui/pickGitFolder.ts
@@ -24,3 +24,57 @@ export const pickGitFolder = async (): Promise<string | undefined | null> => {
         return mainFolders[0];
     }
 };
+
+type ResolveValue = readonly { name: string; path: string }[] | undefined | null;
+type ResolveType = (value?: ResolveValue) => void;
+type RejectType = (reason?: any) => void;
+const showBaseNameQuickInputButton: vscode.QuickInputButton = {
+    iconPath: new vscode.ThemeIcon('pass'),
+    tooltip: vscode.l10n.t('Use base name'),
+};
+const showFullNameQuickInputButton: vscode.QuickInputButton = {
+    iconPath: new vscode.ThemeIcon('pass-filled'),
+    tooltip: vscode.l10n.t('Use full path'),
+};
+
+export const pickMultiFolder = async (gitFolders: string[]): Promise<ResolveValue> => {
+    const fullNameOptions: vscode.QuickPickItem[] = gitFolders.map((folderPath) => ({ label: folderPath }));
+    const baseNameOptions: vscode.QuickPickItem[] = gitFolders.map((folderPath) => ({
+        label: path.basename(folderPath),
+        description: folderPath,
+    }));
+    let resolve: ResolveType = () => {};
+    let reject: RejectType = () => {};
+    let waiting = new Promise<ResolveValue>((_resolve, _reject) => {
+        resolve = _resolve;
+        reject = _reject;
+    });
+    try {
+        const picker = vscode.window.createQuickPick();
+        picker.title = vscode.l10n.t('Select folder(s)');
+        picker.items = baseNameOptions;
+        picker.canSelectMany = true;
+        picker.buttons = [showFullNameQuickInputButton];
+        picker.onDidTriggerButton((event) => {
+            if (event === showFullNameQuickInputButton) {
+                picker.buttons = [showBaseNameQuickInputButton];
+                picker.items = fullNameOptions;
+            } else {
+                picker.buttons = [showFullNameQuickInputButton];
+                picker.items = baseNameOptions;
+            }
+        });
+        picker.onDidHide(() => {
+            resolve();
+            picker.dispose();
+        });
+        picker.onDidAccept(() => {
+            resolve(picker.selectedItems.map((item) => ({ name: item.label, path: item.description || item.label })));
+            picker.dispose();
+        });
+        picker.show();
+        return waiting;
+    } catch (error) {
+        return void 0;
+    }
+};
