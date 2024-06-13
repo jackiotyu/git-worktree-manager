5b92237317c030dc7c7d2cf32e0a36e9b523529a
jackiotyu <2504448153@qq.com>
2024-06-13 14:29:51 +0800

    perf: 优化输入框逻辑

--- src/core/command/renameGitFolderCmd.ts
+++ src/core/command/renameGitFolderCmd.ts
@@ -1,7 +1,8 @@
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 import { getFolderConfig, updateFolderConfig } from '@/core/util/state';
 import { Alert } from '@/core/ui/message';
 import { GitFolderItem } from '@/core/treeView/items';
+import path from 'path';
 
 const pickFolderConfig = (item?: GitFolderItem) => {
     if (!item) return;
@@ -11,26 +12,27 @@ const pickFolderConfig = (item?: GitFolderItem) => {
 export const renameGitFolderCmd = async (item?: GitFolderItem) => {
     if (!item) return;
     let folder = pickFolderConfig(item);
-    if (!folder) {
-        return;
-    }
-    const path = folder.path;
+    if (!folder) return;
+    const folderName = folder.name;
+    const baseName = path.basename(folderName);
+    const valueSelection: [number, number] =
+        baseName === folderName ? [0, folderName.length] : [0, folderName.length - baseName.length];
     let name = await vscode.window.showInputBox({
         title: vscode.l10n.t('Rename the git repository alias'),
         placeHolder: vscode.l10n.t('Enter the name of the repository for the showcase'),
-        value: folder.name,
+        value: folderName,
+        valueSelection: valueSelection,
         validateInput(value) {
             if (!value) {
                 return vscode.l10n.t('Enter the name of the repository for the showcase');
             }
         },
     });
-    if (!name) {
-        return;
-    }
+    if (!name) return;
     folder.name = name;
     let allFolders = getFolderConfig();
-    let index = allFolders.findIndex((i) => i.path === path);
+    const folderPath = folder.path;
+    let index = allFolders.findIndex((i) => i.path === folderPath);
     if (~index) {
         allFolders[index].name = name;
         await updateFolderConfig(allFolders);
