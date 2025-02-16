dad0bbe1eb9880675c4bd7b5ff7a2d96eac764cb
jackiotyu <2504448153@qq.com>
2025-02-16 08:41:37 +0800

    feat(core): prevent focus loss when creating a new branch
    - Add ignoreFocusOut property to input box for creating a new branch
    - This ensures that the input box retains focus when the user interacts with other parts of the IDE
    
    Signed-off-by: jackiotyu <2504448153@qq.com>

--- src/core/ui/inputNewBranch.ts
+++ src/core/ui/inputNewBranch.ts
@@ -8,6 +8,7 @@ const backButton = vscode.QuickInputButtons.Back;
 export const inputNewBranch = async (cwd: string) => {
     const { promise, resolve, reject } = withResolvers<string | undefined | false>();
     const inputBox = vscode.window.createInputBox();
+    inputBox.ignoreFocusOut = true;
     inputBox.value = vscode.workspace.getConfiguration('git').get('branchPrefix', '');
     inputBox.valueSelection = [-1, -1];
     inputBox.placeholder = vscode.l10n.t('Please input branch name');
