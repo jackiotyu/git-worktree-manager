2c382d757708c6adff7ab8a88031387f0ad48e06
jackiotyu <2504448153@qq.com>
2026-01-27 09:06:23 +0800

    fix(command): pass DefaultDisplayList to pickWorktree in searchAllWorktreeCmd
    - Updated the searchAllWorktreeCmd to use DefaultDisplayList.all when calling pickWorktree for improved functionality.

--- src/core/command/searchAllWorktreeCmd.ts
+++ src/core/command/searchAllWorktreeCmd.ts
@@ -1,5 +1,6 @@
 import { pickWorktree } from '@/core/quickPick/pickWorktree';
+import { DefaultDisplayList } from '@/types';
 
 export const searchAllWorktreeCmd = () => {
-    pickWorktree();
+    pickWorktree(DefaultDisplayList.all);
 };
