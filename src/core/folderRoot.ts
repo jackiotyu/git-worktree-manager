6607e83063ae21c784fc8417d50f24b10985a0b9
runner-ccl <964019961@qq.com>
2026-02-13 15:40:43 +0800

    fix(linux): Fix the issue where the plugin does not work properly under Linux

--- src/core/folderRoot.ts
+++ src/core/folderRoot.ts
@@ -13,7 +13,7 @@ class WorkspaceFolderRoot implements vscode.Disposable {
     private checkUri() {
         const folders = vscode.workspace.workspaceFolders || [];
         this._folderSet = new Set(
-            folders.map((i) => i.uri.fsPath.toLocaleLowerCase().replace(/\\/g, '/')).filter((i) => i),
+            folders.map((i) => i.uri.fsPath.replace(/\\/g, '/')).filter((i) => i),
         );
         // TODO Special handling for multiple workspaces
         if (folders.length) {
