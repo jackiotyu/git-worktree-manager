b8a06278c7db82da2ff05b2e89c61d0ba9314759
jackiotyu <2504448153@qq.com>
2026-02-28 13:11:56 +0800

    fix(git): simplify main folder retrieval logic
    Updated the getMainFolder function to streamline the command used for retrieving the main folder path. The function now checks if the result is '.git' and returns the current working directory if true, improving clarity and efficiency.

--- src/core/git/getMainFolder.ts
+++ src/core/git/getMainFolder.ts
@@ -2,11 +2,8 @@ import { execBase } from '@/core/git/exec-base';
 
 export const getMainFolder = async (cwd: string) => {
     try {
-        const { stdout: mainFolderFull } = await execBase(cwd, [
-            'rev-parse',
-            '--path-format=absolute',
-            '--git-common-dir',
-        ]);
+        const { stdout: mainFolderFull } = await execBase(cwd, ['rev-parse', '--git-common-dir']);
+        if (mainFolderFull.trim() === '.git') return cwd;
         return mainFolderFull.trim().replace(/\/.git$/, '');
     } catch {
         return '';
