0f047cc30dd8bb19b7da0ac5ac5f67add39f9070
jackiotyu <2504448153@qq.com>
2026-07-26 13:03:53 +0800

    fix: make "Find Worktree" respect default display list (#51)
    - Modified searchAllWorktreeCmd to accept an argument.
    - If the argument is 'auto', it calls pickWorktree without parameters.
    - Otherwise, it defaults to picking from DefaultDisplayList.all.

--- src/core/command/searchAllWorktreeCmd.ts
+++ src/core/command/searchAllWorktreeCmd.ts
@@ -1,6 +1,7 @@
 import { pickWorktree } from '@/core/quickPick/pickWorktree';
 import { DefaultDisplayList } from '@/types';
 
-export const searchAllWorktreeCmd = () => {
-    pickWorktree(DefaultDisplayList.all);
+export const searchAllWorktreeCmd = (args: unknown) => {
+    if (args === 'auto') pickWorktree();
+    else pickWorktree(DefaultDisplayList.all);
 };
